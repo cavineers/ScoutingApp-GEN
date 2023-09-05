a6e54216782a3940eae43b654672360125234bc9
Max Maginnis <maxmaginnis@yahoo.com>
2023-09-05 11:50:22 -0400

    removed old data util

--- comps/comp2023/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import constants, data_manage, routes
-from ScoutingApp import competition
-
-competition.define(competition.Competition("2023", "2023: Charged Up", "home.html", routes.blueprint))
