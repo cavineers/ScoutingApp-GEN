b21774db0b1a5baabe72be829f6f5ffcb16bf2aa
PalaceLad <aidenjohnmcmanus@gmail.com>
2024-02-17 11:34:17 -0500

    added working documentation pop up to navbar + data manage updates

--- data_manage.py
+++ data_manage.py
@@ -2,17 +2,33 @@ from datetime import datetime, timedelta
 import json
 import os
 import scoutingutil
-from scoutingutil import Column, Table
+from scoutingutil import Column, configs, Table, SheetsService
 
 # constants used for key names
 START = "start"
 END = "end"
-END_AUTO = "end-auto"
+END_AUTO = "endAuto"
 MATCH_INPUT_NAMES = ("score", "move", "pickup", "dropped", "defend")
 
 # directory and file paths
 DIR = os.path.dirname(__file__)
 SUBMISSIONS_FILE = os.path.join(DIR, "submissions.txt")
+
+sheets_api = SheetsService()
+
+def init_sheets_api():
+    if not os.path.isfile(scoutingutil.configs.CONFIG_PATH):
+        raise FileNotFoundError("Must create a config.json file to read from.")
+    cnfg = scoutingutil.configs.load()
+    try:
+        sheets_api.config(cnfg)
+    except Exception as e:
+        token_path = os.path.abspath(cnfg.get(configs.SHEETS_TOKEN_PATH, "token.json"))
+        if os.path.isfile(token_path):
+            os.remove(token_path)
+            sheets_api.config(cnfg)
+        else:
+            raise
 
 # function to parse ISO date string to datetime object
 def parse_isodate(dstr:str):
@@ -38,7 +54,10 @@ def iter_teleop(data, raw:dict[str]):
             yield dt
 
 # function to prepare data by converting ISO date strings to datetime objects
+# im going to kill this stupid start/end def
 def prep_data(data:dict[str]):
+    if "start" not in data or "end" not in data:
+        return
     #set all iso datetime strings to datetime objects
     data[START] = parse_isodate(data[START])
     data[END] = parse_isodate(data[END])
@@ -56,10 +75,14 @@ def prep_data(data:dict[str]):
 # function to handle data uploaded to the server
 def handle_upload(raw:"dict[str]"):
     "Handle data sent to the upload route"
-    #TODO use scoutingutil stuff
-    save_local(raw) #remove this comment if you want to physically see the submission.txt file
+    save_local(raw)
+    
+    # prep_data(raw)
+    
+    row = ScoutingData.process_data(raw)
+    
+    sheets_api.save_to_sheets(row)
 
-# function to save (append) raw data to a local file
 def save_local(raw:"dict[str]|str"):
     "Save (append) the raw data to a local file."
     if not isinstance(raw, str):
@@ -83,26 +106,30 @@ class ScoutingData(Table):
     date = Column("DATE", "date")
     robot = Column("ROBOT", "robot")
     team = Column("TEAM", "team")
-    match = Column("MATCH", "match", process_data=lambda ctx: int(ctx.data), strict=True)
+    match = Column("MATCH", "preliminaryData", process_data=lambda ctx: ctx.data["match"], strict=True)
     scouter = Column("SCOUTER", "scouter")
     
     #prematch page
-    starting_piece = Column("STARTING  PIECE", "startingpiece")
+    starting_piece = Column("STARTING PIECE", "startingpiece")
     starting_position = Column("STARTING POSITION", "startingpos")
     
     #auto page
-    picked_up_note_auto = Column("AUTO:ACTION 1", "pickup", process_data=count_column_auto)
-    missed_shot_auto = Column("AUTO:ACTION 2", "missed", process_data=count_column_auto)
-    dropped_note_auto = Column("AUTO:ACTION 3", "dropped", process_data=count_column_auto)
-    dropped_note_auto = Column("AUTO:ACTION 3", "dropped", process_data=count_column_auto)
+    picked_up_note_auto = Column("AUTO:PICKED UP NOTE", "pickup", process_data=count_column_auto)
+    missed_shot_auto = Column("AUTO:MISSED SHOT", "missed", process_data=count_column_auto)
+    dropped_note_auto = Column("AUTO:DROPPED NOTE", "dropped", process_data=count_column_auto)
     
     #teleop page
-    picked_up_note = Column("ACTION 1", "pickup", process_data=count_column_teleop)
-    missed_shot = Column("ACTION 2", "missed", process_data=count_column_teleop)
-    dropped_note = Column("ACTION 3", "dropped", process_data=count_column_teleop)
-    defense = Column("ACTION 4", "defense", process_data=count_column_teleop)
-
+    picked_up_note = Column("PICKED UP NOTE", "pickup", process_data=count_column_teleop)
+    missed_shot = Column("MISSED SHOT", "missed", process_data=count_column_teleop)
+    dropped_note = Column("DROPPED NOTE", "dropped", process_data=count_column_teleop)
+    defense = Column("DEFENSE", "defense", process_data=count_column_teleop)
+    cooperation = Column("COOPERATION BONUS", "cooperation", process_data=count_column_teleop)
+    amplified = Column("AMPLIFIED BONUS", "amplified", process_data=count_column_teleop)
+    offState = Column("CHAIN STATE", "chainState") 
+    state1 = Column("CHAIN POSITION", "chainPosition")
+    state2 = Column("CHAIN STATE", "chainState") 
+    
     #result page
-    comments = Column("COMMENTS", "comments")
+    comments = Column("COMMENTS", "comments", lambda ctx: ctx.data[0])
     
     #done
